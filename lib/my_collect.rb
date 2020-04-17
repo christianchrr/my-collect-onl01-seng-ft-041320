ec306b670d3de67011b3fc1520d57d3dac64c27f
Christian Cherry <christianchrr@gmail.com>
2020-04-17 20:42:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,9 @@ def my_collect(languages)
   collection = []
   i = 0
   while i < languages.length
-    collection << yield({languages[i]})
-  
+    collection << yield(languages[i])
+    i += 1
+  end
+  return collection
 end
 
